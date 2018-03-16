63c68e98ed8da38c0570cfc5ddf2e9b7a7c41857
natto2020 <37442771+natto2020@users.noreply.github.com>
2018-03-16 20:57:14 +0900

    Update wsgi.py

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -1,10 +1,17 @@
-import os
-import sys
+"""
+WSGI config for mysite project.
 
-path = os.path.expanduser('~/mysite')
-if path not in sys.path:
-    sys.path.append(path)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
+"""
+
+import os
+
 from django.core.wsgi import get_wsgi_application
-from django.contrib.staticfiles.handlers import StaticFilesHandler
-application = StaticFilesHandler(get_wsgi_application())
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+
+application = get_wsgi_application()
+
